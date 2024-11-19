7dc3134aa3fefab9c6159d232d169ae07f880554
candra <zerocreateneet@gmail.com>
2024-11-19 22:28:40 +0800

    Menambahkan file besicBelajar javaScript

---
+++ 06-objectCreat/script.js
@@ -0,0 +1,28 @@
+const methodMahasiswa = {
+	makan: function(porsi){
+		this.energi += porsi;
+		console.log(`Halo ${this.nama}, selamat makan`);
+	},
+	main: function(jam){
+		this.energi += jam;
+		console.log(`Halo ${this.nama}, selamat bermain`);
+	},
+	tidur: function(jam){
+		this.energi += jam * 2;
+		console.log(`Halo ${this.nama}, selamat tidur`);
+	}
+};
+
+
+function Mahasiswa(nama,energi) {
+	let mahasiswa = Object.create(methodMahasiswa);
+	mahasiswa.nama = nama;
+	mahasiswa.energi = energi;
+	mahasiswa.makan = methodMahasiswa.makan;
+	mahasiswa.main = methodMahasiswa.main;
+	mahasiswa.tidur = methodMahasiswa.tidur;
+
+	return mahasiswa;
+};
+
+let data = Mahasiswa ('Candra',10);
